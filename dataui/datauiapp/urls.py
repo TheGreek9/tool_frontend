13aa9d600907263a89d3a073e502c1e91ed4c9b7
Spyridon Ziangos <sziangos@gmail.com>
2018-11-25 15:08:24 -0800

    Add and Change views to include contacts url

--- dataui/datauiapp/urls.py
+++ dataui/datauiapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import IndexView, DetailsView
+from .views import FileView, DetailsView, NextFileView, IndexView
 from . import views
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('cases/', FileView.as_view(), name='cases'),
+    path('contacts/', NextFileView.as_view(), name='contacts'),
     path('details/<slug:file_name>/', DetailsView.as_view(), name='details')
 ]
--- dataui/datauiapp/views.py
+++ dataui/datauiapp/views.py
@@ -10,10 +10,25 @@ from django.views import View
 
 from .models import TestModel
 
+super_long_dicts = {}
+
 
 class IndexView(View):
     def get(self, request):
+        super_long_dicts.update({'caseclient': dict(casename='yooooo',
+                                                    firm_file_number=696969,
+                                                    court_case=True,
+                                                    something_else='whatevs')})
         return render(request, 'datauiapp/index.html')
+
+
+class FileView(View):
+    next_uri = 'cases'
+
+    def get(self, request):
+        super_long_dicts.update({'file_view': self.next_uri})
+        return render(request, 'datauiapp/cases.html', {'next_uri':
+                                                            self.next_uri})
 
     def post(self, request):
         file_name = request.POST['csv_file'].split('.csv')[0]
@@ -35,6 +50,7 @@ class DetailsView(View):
             'model_fields': self.model_fields,
             'file_name': 'next_test',
         }
+        print("******* {}".format(super_long_dicts))
         return render(request, 'datauiapp/csv_table.html', context)
 
     def post(self, request, *args, **kwargs):
@@ -51,7 +67,7 @@ class DetailsView(View):
                 field_attr = getattr(row, request.POST[field], '')
                 if field_attr:
                     giant_dict.update({field: field_attr})
-
+            super_long_dicts.update(giant_dict)
             test_list.append(giant_dict)
 
         return HttpResponse("{}".format(test_list))
@@ -62,3 +78,12 @@ class DetailsView(View):
 
     def get_header_cols(self):
         return [a for a in csv.reader(open(self.file_path, "r"))][0]
+
+
+class NextFileView(FileView):
+    next_uri = 'contacts'
+
+    def post(self, request):
+        print("******* {}".format(super_long_dicts))
+        file_name = request.POST['csv_file'].split('.csv')[0]
+        return HttpResponse("Lick, lick, lick my balls!")
